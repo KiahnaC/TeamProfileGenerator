5becf42e593c6b41b0bc7dbed2a660ad0d5a3067
Kiahna Carmack <Kiahna.carmack@gmail.com>
2022-03-01 17:03:41 -0800

    added syntax for prompts and inquirier

---
+++ userPrompts/Manager.js
@@ -0,0 +1,16 @@
+const Employee = require('./Employee');
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        // calling employee constructor 
+        super(name, id, email,officeNumber);
+
+        this.officeNumber = officeNumber;
+    }
+
+    getRole () {
+        return 'Manager';
+    }
+};
+
+module.exports= Manager;
